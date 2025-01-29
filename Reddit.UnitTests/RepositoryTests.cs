6fb1d91e09e178c4a9953e94edeec0d3348ce6aa
PC <PC@B14-11>
2025-01-29 18:27:11 +0400

    tests for pagedList

--- Reddit.UnitTests/RepositoryTests.cs
+++ Reddit.UnitTests/RepositoryTests.cs
@@ -8,7 +8,7 @@ public class RepositoryTests
 {
     private IPostsRepository GetPostsRepostory()
     {
-        var dbName = Guid.NewGuid().ToString();     // give unqie name to the database, so that different tests don't interfere with each other
+        var dbName = Guid.NewGuid().ToString();    
         var options = new DbContextOptionsBuilder<ApplicationDbContext>()
             .UseInMemoryDatabase(databaseName: dbName)
             .Options;
@@ -17,8 +17,9 @@ public class RepositoryTests
         dbContext.Posts.Add(new Post { Title = "Title 1", Content = "Content 1", Upvote = 5, Downvote = 1 });
         dbContext.Posts.Add(new Post { Title = "Title 2", Content = "Content 1", Upvote = 12, Downvote = 1 });
         dbContext.Posts.Add(new Post { Title = "Title 3", Content = "Content 1", Upvote = 3, Downvote = 1 });
-        dbContext.Posts.Add(new Post { Title = "Title 4", Content = "Content 1", Upvote = 221, Downvote = 1 }); // 221 / 222 = 0.9954954954954955
-        dbContext.Posts.Add(new Post { Title = "Title 5", Content = "Content 1", Upvote = 5, Downvote = 2123 }); // 5 / 2123 = 0.002356344
+        dbContext.Posts.Add(new Post { Title = "Title 4", Content = "Content 1", Upvote = 221, Downvote = 1 }); 
+        dbContext.Posts.Add(new Post { Title = "Title 5", Content = "Content 1", Upvote = 5, Downvote = 2123 }); 
+        dbContext.Posts.Add(new Post { Title = "Title 6", Content = "Content 1", Upvote = 6, Downvote = 2122 }); 
         dbContext.SaveChanges();
         return new PostsRepository(dbContext);
     }
@@ -28,20 +29,20 @@ public class RepositoryTests
         var postsRepository = GetPostsRepostory();
         var posts = await postsRepository.GetPosts(1, 2, null, null, false);
         Assert.Equal(2, posts.Items.Count);
-        Assert.Equal(5, posts.TotalCount);
+        Assert.Equal(6, posts.TotalCount);
         Assert.True(posts.HasNextPage);
         Assert.False(posts.HasPreviousPage);
     }
+
     [Fact]
     public async Task GetPosts_ReturnsCorrect()
     {
         var postsRepository = GetPostsRepostory();
         var posts = await postsRepository.GetPosts(1, 2, null, "popular", false);
         Assert.Equal(2, posts.Items.Count);
-        Assert.Equal(5, posts.TotalCount);
+        Assert.Equal(6, posts.TotalCount);
         Assert.True(posts.HasNextPage);
         Assert.False(posts.HasPreviousPage);
-        //  Positivity -> Assert.Equal("Title 4", posts.Items.First().Title); 
         Assert.Equal("Title 5", posts.Items.First().Title);
     }
     
@@ -53,10 +54,24 @@ public class RepositoryTests
         Assert.Equal("pageNumber", exception.ParamName);
     }
     [Fact]
-    public async Task GetPosts_InvalidPageSize_ThrowsArgumentOutOfRangeException()
+    public async Task GetPosts_InvalidPageSize_ThrowsArgumentOutOfRangeException_When_Page_Size_Is_Zero()
     {
         var repository = GetPostsRepostory();
         var exception = await Assert.ThrowsAsync<ArgumentOutOfRangeException>(() => repository.GetPosts(pageNumber: 1, pageSize: 0, searchTerm: null, sortTerm: null));
         Assert.Equal("pageSize", exception.ParamName);
+    }
+    [Fact]
+    public async Task GetPosts_InvalidPageSize_ThrowsArgumentOutOfRangeException_When_Page_Size_Is_Negative()
+    {
+        var repository = GetPostsRepostory();
+        var exception = await Assert.ThrowsAsync<ArgumentOutOfRangeException>(() => repository.GetPosts(pageNumber: 1, pageSize: -1, searchTerm: null, sortTerm: null));
+        Assert.Equal("pageSize", exception.ParamName);
+    }   
+    [Fact]
+    public async Task GetPosts_InvalidPageSize_ThrowsArgumentOutOfRangeException_When_Page_Is_Out_Of_Range()
+    {
+        var repository = GetPostsRepostory();
+        var exception = await Assert.ThrowsAsync<ArgumentOutOfRangeException>(() => repository.GetPosts(pageNumber: 0, pageSize: 2, searchTerm: null, sortTerm: null));
+        Assert.Equal("pageNumber", exception.ParamName);
     }
 }
